37de13c780fc190458926f01423da77a23197ff2
Himanshu kumar <hk225064@gmail.com>
2025-09-27 14:22:19 +0530

    fix bug

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin:[process.env.FRONTEND_CLIENT_URL],                                                                                                          credentials:true
+    origin:[process.env.FRONTEND_CLIENT_URL],                                                     credentials:true
 }));
 
 app.use(cookieParser())
